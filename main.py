7f589a5887284a2ec7523cfc0a8c2779d3f5e284
BaAniq <basia.anisimowicz@gmail.com>
2024-03-17 12:58:48 +0100

    Add a part that saves template if is person's bday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import pandas
-
+import random
 my_email = "dimitoret@gmail.com"
 my_password = "ForPython12"
 
@@ -10,6 +10,16 @@ month_today = today.month
 
 birthday_data = pandas.read_csv("birthdays.csv")
 
+for (index, row) in birthday_data.iterrows():
 
+    if row.month == month_today and row.day == day_today:
+        print(row)
+        num_letter = random.choice(range(1, 4))
+        with open(f'letter_templates/letter_{num_letter}.txt') as letter:
+
+            letter_str = letter.read()
+            letter_str = letter_str.replace('[NAME]', row.first_name)
+            with open(f'email_to_{row.first_name}', 'w+') as new_letter:
+                new_letter.write(letter_str)
 
 
