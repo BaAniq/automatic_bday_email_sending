9b00a0c1d2026db04e1acfa3441608dfbf394e94
BaAniq <basia.anisimowicz@gmail.com>
2024-03-17 13:00:34 +0100

    Add a part which sends email to the person

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import datetime as dt
 import pandas
 import random
+import smtplib
 my_email = "dimitoret@gmail.com"
-my_password = "ForPython12"
+my_password = "jonq cuqh zjgt trqe "
 
 today = dt.datetime.now()
 day_today = today.day
@@ -13,7 +14,6 @@ birthday_data = pandas.read_csv("birthdays.csv")
 for (index, row) in birthday_data.iterrows():
 
     if row.month == month_today and row.day == day_today:
-        print(row)
         num_letter = random.choice(range(1, 4))
         with open(f'letter_templates/letter_{num_letter}.txt') as letter:
 
@@ -21,5 +21,11 @@ for (index, row) in birthday_data.iterrows():
             letter_str = letter_str.replace('[NAME]', row.first_name)
             with open(f'email_to_{row.first_name}', 'w+') as new_letter:
                 new_letter.write(letter_str)
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=my_email, to_addrs=row.email, msg=f'Subject:Happy Birthday!\n\n{letter_str}')
+
 
 
