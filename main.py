2a1dad2dbd9b2233f0c8cf414699dc27287662a6
BaAniq <basia.anisimowicz@gmail.com>
2024-03-17 12:09:22 +0100

    Create new parameters

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import datetime as dt
+import pandas
+
+my_email = "dimitoret@gmail.com"
+my_password = "ForPython12"
+
+today = dt.datetime.now()
+day_today = today.day
+month_today = today.month
+
+birthday_data = pandas.read_csv("birthdays.csv")
+
+
 
 
